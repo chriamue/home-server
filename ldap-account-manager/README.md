ef26f5ab95768f1670c3b680c2c82289cd597951
chriamue <chriamue@gmail.com>
2023-12-03 10:13:54 +0100

    :fire: use official ldap account manager docker image

--- ldap-account-manager/README.md
+++
@@ -1,7 +0,0 @@
-# ldap-account-manager
-
-The Dockerfile is inspired by [aexo/docker-ldap-account-manager](https://github.com/aexo/docker-ldap-account-manager)
-
-## Usage
-
-Open ldap account manager in browser: [http://lam.localhost]()
--- ldap-account-manager/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -z "$(ls -A "/var/lib/ldap-account-manager")" ]; then
-   cp -r /opt/lam/* /var/lib/ldap-account-manager/
-   chown www-data:www-data -R /var/lib/ldap-account-manager/*
-fi
-
-/usr/sbin/apache2ctl -D FOREGROUND
